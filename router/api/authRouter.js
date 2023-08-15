c939b7c4c38d4bc66ea2ad787ac3a1dc28f949d5
veremeychukden <veremeychukden@gmail.com>
2023-08-15 17:24:17 +0300

    22

--- router/api/authRouter.js
+++ router/api/authRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const AuthController = require("../../controllers/authController");
+const AuthController = require("../../controllers/AuthController");
 
 router.post('/login', AuthController.login);
 router.post('/signUp', AuthController.signUp);
